66ab934fbe57b6d7759a556429c5463adc4711dd
panimir <panov.volodimir@stud.onu.edu.ua>
2022-05-26 22:33:33 +0300

    Created 'function' files

---
+++ function.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "function.h"
+
+class Function
+{
+	class LineralFunc
+	{};
+
+	class Sin
+	{};
+
+	class Cos
+	{};
+
+	class Square
+	{};
+
+	class Func_sum
+	{};
+
+	class Func_prod
+	{};
+};
