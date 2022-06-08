617bff68b43c30d39f797364dec40ff16ddd8fdd
panimir <panov.volodimir@stud.onu.edu.ua>
2022-06-08 11:09:22 +0300

    Created makefile and basic classes

---
+++ classes/Function.h
@@ -0,0 +1,11 @@
+#ifndef FUNCITONS_FUNC
+#define FUNCTIONS_FUNC
+
+class Function
+{
+public:
+	virtual void printFunc() = 0;
+	virtual void info() = 0;
+}
+
+#endif // FUNCTIONS_FUNC
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
+using std::cin, std::cout;
+
 int main()
 {
-
+	cout << "Hello, course work!\n";
 	return 0;
 }
