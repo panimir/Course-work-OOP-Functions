f06d86bb12b3c5539186d2f52c5da9f71ef17d0d
panimir <panov.volodimir@stud.onu.edu.ua>
2022-06-15 12:29:35 +0300

    Added fine interface and readme

--- main.cpp
+++ main.cpp
@@ -4,11 +4,9 @@
 
 #include "classes/Cos.hpp"
 #include "classes/Linear.hpp"
+#include "classes/Oper.hpp"
 #include "classes/Power.hpp"
 #include "classes/Sin.hpp"
-#include "classes/Oper.hpp"
-
-using namespace std;
 
 void linearBranch();
 void powerBranch();
@@ -56,60 +54,60 @@ int main()
 char firstChoise()
 {
 	char pr;
-	cout << "What do you want to do?\n"
-		 << "1. Make linear function \n"
-		 << "2. Make power function \n"
-		 << "3. Make sin function \n"
-		 << "4. Make cos function \n"
-		 << "5. Calculate sum of functions \n"
-		 << "6. Calculate multiplication of functions \n"
-		 << "7. Calculate difference of functions \n"
-		 << "0. Exit \n"
-		 << "=> ";
-	cin >> pr;
+	std::cout << "What do you want to do?\n"
+			  << "1. Make linear function \n"
+			  << "2. Make power function \n"
+			  << "3. Make sin function \n"
+			  << "4. Make cos function \n"
+			  << "5. Calculate sum of functions \n"
+			  << "6. Calculate multiplication of functions \n"
+			  << "7. Calculate difference of functions \n"
+			  << "0. Exit \n"
+			  << "=> ";
+	std::cin >> pr;
 	return pr;
 }
 
 char secondChoise()
 {
 	char pr;
-	cout << "What do you want to do next? \n"
-		 << "1. Print funciton \n"
-		 << "2. Print function info \n"
-		 << "3. Find derivative \n"
-		 << "4. Calculate function \n"
-		 << "0. Create another function \n"
-		 << "=> ";
-	cin >> pr;
+	std::cout << "What do you want to do next? \n"
+			  << "1. Print funciton \n"
+			  << "2. Print function info \n"
+			  << "3. Find derivative \n"
+			  << "4. Calculate function \n"
+			  << "0. Create another function \n"
+			  << "=> ";
+	std::cin >> pr;
 	return pr;
 }
 
 void linearBranch()
 {
 	int s, c;
-	cout << "Type slope and coefficient for linear funciton.\n=> ";
-	cin >> s >> c;
+	std::cout << "Type slope and coefficient for linear funciton.\n=> ";
+	std::cin >> s >> c;
 	Linear linear(s, c);
-	cout << "The function is: ";
-	cout << linear.printFunc() << '\n';
+	std::cout << "\n=> The function is: ";
+	std::cout << linear.printFunc() << '\n' << '\n';
 	char pr = 'x';
 	while (pr != '0') {
 		pr = secondChoise();
 		switch (pr) {
 		case '1':
-			cout << linear.printFunc() << '\n';
+			std::cout << "\n=> " << linear.printFunc() << '\n' << '\n';
 			break;
 		case '2':
-			cout << linear.info() << '\n';
+			std::cout << "\n=> " << linear.info() << '\n' << '\n';
 			break;
 		case '3':
-			cout << linear.deriv() << '\n';
+			std::cout << "\n=> " << linear.deriv() << '\n' << '\n';
 			break;
 		case '4':
 			int x;
-			cout << "Type value for 'x'.\n=> ";
-			cin >> x;
-			cout << linear.calc(x) << '\n';
+			std::cout << "Type value for 'x'.\n=> ";
+			std::cin >> x;
+			std::cout << "\n=> " << linear.calc(x) << '\n' << '\n';
 			break;
 		}
 	}
@@ -119,29 +117,30 @@ void linearBranch()
 void powerBranch()
 {
 	int s, c, d;
-	cout << "Type slope, coefficient and degree for funciton.\n=> ";
-	cin >> s >> c >> d;
+	std::cout << "Type slope, coefficient and degree for funciton.\n=> ";
+	std::cin >> s >> c >> d;
 	Power power(s, c, d);
-	cout << "The function is: ";
-	cout << power.printFunc() << '\n';
+	std::cout << "\n=> "
+			  << "The function is: ";
+	std::cout << power.printFunc() << '\n' << '\n';
 	char pr = 'x';
 	while (pr != '0') {
 		pr = secondChoise();
 		switch (pr) {
 		case '1':
-			cout << power.printFunc() << '\n';
+			std::cout << "\n=> " << power.printFunc() << '\n' << '\n';
 			break;
 		case '2':
-			cout << power.info() << '\n';
+			std::cout << "\n=> " << power.info() << '\n' << '\n';
 			break;
 		case '3':
-			cout << power.deriv() << '\n';
+			std::cout << "\n=> " << power.deriv() << '\n' << '\n';
 			break;
 		case '4':
 			int x;
-			cout << "Type value for 'x'.\n=> ";
-			cin >> x;
-			cout << power.calc(x) << '\n';
+			std::cout << "Type value for 'x'.\n=> ";
+			std::cin >> x;
+			std::cout << "\n=> " << power.calc(x) << '\n' << '\n';
 			break;
 		}
 	}
@@ -151,26 +150,27 @@ void powerBranch()
 void sinBranch()
 {
 	int v;
-	cout << "Type value for sin.\n=> ";
-	cin >> v;
+	std::cout << "Type value for sin.\n=> ";
+	std::cin >> v;
 	Sin sin(v);
-	cout << "The function is: ";
-	cout << sin.printFunc() << '\n';
+	std::cout << "\n=> "
+			  << "The function is: ";
+	std::cout << sin.printFunc() << '\n' << '\n';
 	char pr = 'x';
 	while (pr != '0') {
 		pr = secondChoise();
 		switch (pr) {
 		case '1':
-			cout << sin.printFunc() << '\n';
+			std::cout << "\n=> " << sin.printFunc() << '\n' << '\n';
 			break;
 		case '2':
-			cout << sin.info() << '\n';
+			std::cout << "\n=> " << sin.info() << '\n' << '\n';
 			break;
 		case '3':
-			cout << sin.deriv() << '\n';
+			std::cout << "\n=> " << sin.deriv() << '\n' << '\n';
 			break;
 		case '4':
-			cout << sin.calc(v) << '\n';
+			std::cout << "\n=> " << sin.calc(v) << '\n' << '\n';
 			break;
 		}
 	}
@@ -180,26 +180,27 @@ void sinBranch()
 void cosBranch()
 {
 	int v;
-	cout << "Type value for cos.\n=> ";
-	cin >> v;
+	std::cout << "Type value for cos.\n=> ";
+	std::cin >> v;
 	Cos cos(v);
-	cout << "The function is: ";
-	cout << cos.printFunc() << '\n';
+	std::cout << "\n=> "
+			  << "The function is: ";
+	std::cout << cos.printFunc() << '\n' << '\n';
 	char pr = 'x';
 	while (pr != '0') {
 		pr = secondChoise();
 		switch (pr) {
 		case '1':
-			cout << cos.printFunc() << '\n';
+			std::cout << "\n=> " << cos.printFunc() << '\n' << '\n';
 			break;
 		case '2':
-			cout << cos.info() << '\n';
+			std::cout << "\n=> " << cos.info() << '\n' << '\n';
 			break;
 		case '3':
-			cout << cos.deriv() << '\n';
+			std::cout << "\n=> " << cos.deriv() << '\n' << '\n';
 			break;
 		case '4':
-			cout << cos.calc(v) << '\n';
+			std::cout << "\n=> " << cos.calc(v) << '\n' << '\n';
 			break;
 		}
 	}
@@ -209,33 +210,39 @@ void cosBranch()
 void sumBranch()
 {
 	Oper oper;
-	string f1, f2;
-	cout << "Type f1(x) = ";
-	cin >> f1;
-	cout << "Type f2(x) = ";
-	cin >> f2;
-	cout << "f1(x)+f2(x) = " << oper.getSum(f1, f2) << '\n';
+	std::string f1, f2;
+	std::cout << "Type f1(x) = ";
+	std::cin >> f1;
+	std::cout << "Type f2(x) = ";
+	std::cin >> f2;
+	std::cout << "\n=> "
+			  << "f1(x)+f2(x) = " << oper.getSum(f1, f2) << '\n'
+			  << '\n';
 }
 
 void multBranch()
 {
 	Oper oper;
-	string f1, f2;
-	cout << "Type f1(x) = ";
-	cin >> f1;
-	cout << "Type f2(x) = ";
-	cin >> f2;
-	cout << "f1(x)*f2(x) = " << oper.getMult(f1, f2) << '\n';
+	std::string f1, f2;
+	std::cout << "Type f1(x) = ";
+	std::cin >> f1;
+	std::cout << "Type f2(x) = ";
+	std::cin >> f2;
+	std::cout << "\n=> "
+			  << "f1(x)*f2(x) = " << oper.getMult(f1, f2) << '\n'
+			  << '\n';
 }
 
 void divBranch()
 {
-	string f1, f2;
-	cout << "Type f1(x) = ";
-	cin >> f1;
-	cout << "Type f2(x) = ";
-	cin >> f2;
+	std::string f1, f2;
+	std::cout << "Type f1(x) = ";
+	std::cin >> f1;
+	std::cout << "Type f2(x) = ";
+	std::cin >> f2;
 	Oper of1(f1);
-	Oper of3 = of1-f2;
-	cout << "f1(x)-f2(x) = " << of3.getStr() << '\n';
+	Oper of3 = of1 - f2;
+	std::cout << "\n=> "
+			  << "f1(x)-f2(x) = " << of3.getStr() << '\n'
+			  << '\n';
 }
